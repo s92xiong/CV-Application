ffe68c309bd157cbfd7466c2855f7ce27d78a5ff
Sheuh Xiong <sheuhxiong@gmail.com>
2020-10-07 19:57:21 -0400

    Updates

---
+++ precache-manifest.30e8fad777f7d711a2af41c0d0ea62e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8152ab7245949dafda919e9c3e8025",
+    "url": "/CV-Application/index.html"
+  },
+  {
+    "revision": "5fd14e4041d237b524fb",
+    "url": "/CV-Application/static/css/main.ebf20ffd.chunk.css"
+  },
+  {
+    "revision": "723a5a3969f5778c0d3e",
+    "url": "/CV-Application/static/js/2.c7cf3b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-Application/static/js/2.c7cf3b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd14e4041d237b524fb",
+    "url": "/CV-Application/static/js/main.e4317274.chunk.js"
+  },
+  {
+    "revision": "92afbdb9a6fe30eacf25",
+    "url": "/CV-Application/static/js/runtime-main.b222b860.js"
+  },
+  {
+    "revision": "2a2b6c5afb8f91c11432fdd5775c90f5",
+    "url": "/CV-Application/static/media/profile.2a2b6c5a.png"
+  }
+]);
